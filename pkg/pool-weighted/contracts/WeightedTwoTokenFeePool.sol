612837e69f0ba66e3d39c3f6ab333b2f1fcebc99
0xBriz <chaindevone@gmail.com>
2023-01-14 14:00:49 -0500

    setup swap fee for testing

--- pkg/pool-weighted/contracts/WeightedTwoTokenFeePool.sol
+++ pkg/pool-weighted/contracts/WeightedTwoTokenFeePool.sol
@@ -21,10 +21,40 @@ import "./BaseWeightedPool.sol";
 contract WeightedTwoTokenFeePool is BaseWeightedPool, WeightedPoolProtocolFees {
     using FixedPoint for uint256;
 
+    struct NewPoolParams {
+        string name;
+        string symbol;
+        IERC20[] tokens;
+        uint256[] normalizedWeights;
+        IRateProvider[] rateProviders;
+        address[] assetManagers;
+        uint256 swapFeePercentage;
+    }
+
+    struct FeePoolConfig {
+        uint256 buyFee;
+        uint256 sellFee;
+        uint256 bptJoinFee; // not sure if this something to keep
+        uint256 bptExitFee;
+        address coreToken;
+        address feeReceiver;
+    }
+
     uint256 private constant _MAX_TOKENS = 2;
+    uint256 private constant _MAX_BUY_FEE = 10000;
+    uint256 private constant _MAX_SELL_FEE = 25000;
+    uint256 private constant _MAX_TOTAL_FEE = _MAX_BUY_FEE + _MAX_SELL_FEE;
+    uint256 private constant _MAX_BPT_JOIN_FEE = 10000;
+    uint256 private constant _MAX_BPT_EXIT_FEE = 10000;
+    uint256 private constant _FEE_DENOMINATOR = 1000;
+
+    FeePoolConfig private _poolConfig;
 
     IERC20 internal immutable _token0;
     IERC20 internal immutable _token1;
+
+    address internal immutable _coreToken;
+    uint256 internal immutable _coreTokenIndex;
 
     // All token balances are normalized to behave as if the token had 18 decimals. We assume a token's decimals will
     // not change throughout its lifetime, and store the corresponding scaling factor for each at construction time.
@@ -36,23 +66,14 @@ contract WeightedTwoTokenFeePool is BaseWeightedPool, WeightedPoolProtocolFees {
     uint256 internal immutable _normalizedWeight0;
     uint256 internal immutable _normalizedWeight1;
 
-    struct NewPoolParams {
-        string name;
-        string symbol;
-        IERC20[] tokens;
-        uint256[] normalizedWeights;
-        IRateProvider[] rateProviders;
-        address[] assetManagers;
-        uint256 swapFeePercentage;
-    }
-
     constructor(
         NewPoolParams memory params,
         IVault vault,
         IProtocolFeePercentagesProvider protocolFeeProvider,
         uint256 pauseWindowDuration,
         uint256 bufferPeriodDuration,
-        address owner
+        address owner,
+        FeePoolConfig memory poolConfig
     )
         BaseWeightedPool(
             vault,
@@ -75,6 +96,26 @@ contract WeightedTwoTokenFeePool is BaseWeightedPool, WeightedPoolProtocolFees {
         _require(params.tokens.length == 2, Errors.MAX_TOKENS);
         InputHelpers.ensureInputLengthMatch(_MAX_TOKENS, params.normalizedWeights.length);
 
+        // validate config
+        _require(poolConfig.coreToken != address(0), Errors.ZERO_TOKEN);
+        require(poolConfig.buyFee <= _MAX_BUY_FEE, "Buy fee too high");
+        require(poolConfig.sellFee <= _MAX_SELL_FEE, "Sell fee too high");
+        require(poolConfig.bptJoinFee <= _MAX_BPT_JOIN_FEE, "BPT join fee too high");
+        require(poolConfig.bptExitFee <= _MAX_BPT_EXIT_FEE, "BPT exit fee too high");
+        require(poolConfig.feeReceiver != address(0), "Fee receiver not set");
+
+        _poolConfig = poolConfig;
+
+        // Set tokens
+        _coreToken = poolConfig.coreToken;
+        _coreTokenIndex = address(params.tokens[0]) == address(poolConfig.coreToken) ? 0 : 1;
+
+        _token0 = params.tokens[0]; // these zero checked somewhere back in the chain?
+        _token1 = params.tokens[1];
+
+        _scalingFactor0 = _computeScalingFactor(params.tokens[0]);
+        _scalingFactor1 = _computeScalingFactor(params.tokens[1]);
+
         // Set weights
         uint256 normalizedWeight0 = params.normalizedWeights[0];
         uint256 normalizedWeight1 = params.normalizedWeights[1];
@@ -85,16 +126,41 @@ contract WeightedTwoTokenFeePool is BaseWeightedPool, WeightedPoolProtocolFees {
 
         _normalizedWeight0 = normalizedWeight0;
         _normalizedWeight1 = normalizedWeight1;
-
-        // Set tokens
-        _token0 = params.tokens[0]; // these zero checked somewhere back in the chain?
-        _token1 = params.tokens[1];
-
-        _scalingFactor0 = _computeScalingFactor(params.tokens[0]);
-        _scalingFactor1 = _computeScalingFactor(params.tokens[1]);
     }
 
-    // Virtual override functions
+    function getPoolConfig() public view returns (FeePoolConfig memory) {
+        return _poolConfig;
+    }
+
+    // Swap overrides
+
+    function _onSwapGivenIn(
+        SwapRequest memory swapRequest,
+        uint256 currentBalanceTokenIn,
+        uint256 currentBalanceTokenOut
+    ) internal override returns (uint256) {
+        uint256 amountOut = super._onSwapGivenIn(swapRequest, currentBalanceTokenIn, currentBalanceTokenOut);
+        (uint256 feeAmount, ) = getSwapFeeForAction(swapRequest, _poolConfig);
+
+        return amountOut - feeAmount;
+    }
+
+    function getSwapFeeForAction(SwapRequest memory swapRequest, FeePoolConfig memory feeConfig)
+        public
+        pure
+        returns (uint256 feeAmount, bool isSell)
+    {
+        // Is it a buy or sell of the token
+        // GivenIn so if token in is core token then we know it's a sell
+        isSell = address(swapRequest.tokenIn) == feeConfig.coreToken;
+        uint256 fee = isSell ? feeConfig.sellFee : feeConfig.buyFee;
+
+        // What are the "scaling" concerns here?
+        // Base contract will scale down before going back to the vault
+        feeAmount = swapRequest.amount.mulUp(fee).divDown(_FEE_DENOMINATOR);
+    }
+
+    // Join/Exit overrides
 
     function _onInitializePool(
         bytes32 poolId,
@@ -103,7 +169,11 @@ contract WeightedTwoTokenFeePool is BaseWeightedPool, WeightedPoolProtocolFees {
         uint256[] memory scalingFactors,
         bytes memory userData
     ) internal virtual override returns (uint256, uint256[] memory) {
-        // TODO: Any setup here?
+        // TODO: Any other setup concerns here?
+
+        // Initialize `_athRateProduct` if the Pool will pay protocol fees on yield.
+        // Not initializing this here properly will cause all joins/exits to revert.
+        if (!_isExemptFromYieldProtocolFees()) _updateATHRateProduct(_getRateProduct(_getNormalizedWeights()));
 
         return super._onInitializePool(poolId, sender, recipient, scalingFactors, userData);
     }
