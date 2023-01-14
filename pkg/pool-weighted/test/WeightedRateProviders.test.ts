d92c3e0b2878f919c11527308778ecfa6225bc6a
0xBriz <chaindevone@gmail.com>
2023-01-14 10:30:58 -0500

    added test for target initial price

--- pkg/pool-weighted/test/WeightedRateProviders.test.ts
+++ pkg/pool-weighted/test/WeightedRateProviders.test.ts
@@ -1,7 +1,7 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { ethers } from 'hardhat';
 import WeightedPool from '@balancer-labs/v2-helpers/src/models/pools/weighted/WeightedPool';
-import { fp, toFp } from '@balancer-labs/v2-helpers/src/numbers';
+import { fp, fromFp, toFp } from '@balancer-labs/v2-helpers/src/numbers';
 import { sharedBeforeEach } from '@balancer-labs/v2-common/sharedBeforeEach';
 import { WeightedPoolType } from '@balancer-labs/v2-helpers/src/models/pools/weighted/types';
 import TokenList from '@balancer-labs/v2-helpers/src/models/tokens/TokenList';
@@ -9,13 +9,16 @@ import { Contract } from 'ethers';
 import { deploy } from '@balancer-labs/v2-helpers/src/contract';
 import Token from '@balancer-labs/v2-helpers/src/models/tokens/Token';
 import { expect } from 'chai';
-import { parseUnits } from 'ethers/lib/utils';
-import { range } from 'lodash';
+import { formatEther } from 'ethers/lib/utils';
+import { calcOutGivenIn, calculateInvariant } from '@balancer-labs/v2-helpers/src/models/pools/weighted/math';
+import { expectEqualWithError } from '@balancer-labs/v2-helpers/src/test/relativeError';
 
 describe('WeightedRateProviders', function () {
   let tokens: TokenList;
   let lp: SignerWithAddress;
   let rateProviders: Contract[];
+  let cGoldRateProvider: Contract;
+  let pool: WeightedPool;
 
   const MAX_TOKENS = 2;
 
@@ -36,11 +39,12 @@ describe('WeightedRateProviders', function () {
   const CGOLD_TARGET_PRICE = CGOLD_RATIO_TO_GOLD.mul(PRICE_ONE_OZ_OF_GOLD);
 
   // Weights and balances set for 80/20 going for ~$64.30 CGOLD token value at start
-  // const WEIGHTS = [parseUnits('0.8'), parseUnits('0.2')];
-  const WEIGHTS = range(1000, 1000 + MAX_TOKENS);
+  const WEIGHTS = [80, 20];
 
-  // $10k in * 0.8 = 8000 / 64.3 target price = 124.4167..
-  const initialBalances = [fp(124.416796), fp(2000)];
+  // $10k in * 0.8 = 8000 / 64.3(target price) = 124.4167.. initial balance in
+  // using calcOutGivenIn actual amount to use for precision is ~121.9 = 64.303402...
+  // Target price division results in 64.301445..., close enough
+  const initialBalances = [fp(121.9), fp(2000)];
 
   const POOL_SWAP_FEE_PERCENTAGE = fp(0.01);
 
@@ -48,28 +52,41 @@ describe('WeightedRateProviders', function () {
     [, lp] = await ethers.getSigners();
   });
 
-  describe('deploying the pool', async () => {
-    let pool: WeightedPool;
-
+  sharedBeforeEach('deploy pool', async () => {
     tokens = await TokenList.create(2, { sorted: true });
-    CGOLD = tokens.first;
-
     rateProviders = await tokens.asyncMap(() => deploy('v2-pool-utils/MockRateProvider'));
 
-    sharedBeforeEach('deploy pool', async () => {
-      pool = await WeightedPool.create({
-        poolType: WeightedPoolType.WEIGHTED_POOL,
-        tokens,
-        weights: WEIGHTS,
-        swapFeePercentage: POOL_SWAP_FEE_PERCENTAGE,
-        rateProviders: rateProviders.map((r) => r.address),
-      });
+    CGOLD = tokens.first;
+    cGoldRateProvider = rateProviders[0];
 
-      await pool.init({ initialBalances, recipient: lp });
+    pool = await WeightedPool.create({
+      poolType: WeightedPoolType.WEIGHTED_POOL,
+      tokens,
+      weights: WEIGHTS,
+      swapFeePercentage: POOL_SWAP_FEE_PERCENTAGE,
+      rateProviders: rateProviders.map((r) => r.address),
     });
+
+    await pool.init({ initialBalances, recipient: lp });
   });
 
-  it('should ', () => {
-    expect(true).to.be.true;
+  it('should add initial liquidity close to target price', async () => {
+    const [poolTokens, normalizedWeights] = await Promise.all([
+      pool.vault.getPoolTokens(pool.poolId),
+      pool.getNormalizedWeights(),
+    ]);
+
+    const amountOut = calcOutGivenIn(
+      poolTokens.balances[0],
+      normalizedWeights[0],
+      poolTokens.balances[1],
+      normalizedWeights[1],
+      fp(1)
+    );
+
+    // const invariant = calculateInvariant(poolTokens.balances, normalizedWeights);
+    // console.log('invariant: ' + formatEther(invariant));
+
+    expectEqualWithError(fp(amountOut), fp(CGOLD_TARGET_PRICE), 0.001);
   });
 });
