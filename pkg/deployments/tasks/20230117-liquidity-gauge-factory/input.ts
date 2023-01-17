672d0972b18e82161e85f75795485b158a272920
0xBriz <chaindevone@gmail.com>
2023-01-17 11:54:39 -0500

    new gauge factory testnet deployment

--- pkg/deployments/tasks/20230117-liquidity-gauge-factory/input.ts
+++ pkg/deployments/tasks/20230117-liquidity-gauge-factory/input.ts
@@ -6,10 +6,9 @@ export type LiquidityGaugeFactoryDeployment = {
   VotingEscrowDelegationProxy: string;
 };
 
-const AuthorizerAdaptor = new Task('20221229-vault', TaskMode.READ_ONLY).output().AuthorizerAdaptor;
-const BalancerMinter = new Task('20230112', TaskMode.READ_ONLY).output().BalancerMinter;
-const VotingEscrowDelegationProxy = new Task('20230117-ve-delegation', TaskMode.READ_ONLY).output()
-  .VotingEscrowDelegationProxy;
+const AuthorizerAdaptor = new Task('20221229-vault', TaskMode.READ_ONLY);
+const BalancerMinter = new Task('20230112-balancer-minter', TaskMode.READ_ONLY);
+const VotingEscrowDelegationProxy = new Task('20230117-ve-delegation', TaskMode.READ_ONLY);
 
 export default {
   AuthorizerAdaptor,
