336842317e85dfd70199e673d5bd38dfaa5afd4d
0xBriz <chaindevone@gmail.com>
2023-01-17 17:08:42 -0500

    gov token. sleep timer for verification attempts

--- pkg/deployments/tasks/20230117-governance-token/index.ts
+++ pkg/deployments/tasks/20230117-governance-token/index.ts
@@ -1,7 +1,11 @@
 import Task from '../../src/task';
 import { TaskRunOptions } from '../../src/types';
+import { sleep } from '../../src/utils';
 
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
-  const governanceToken = await task.deployAndVerify('GovernanceToken', ['Vertek', 'VRTK'], from, force);
+  const args = ['Vertek', 'VRTK'];
+  const governanceToken = await task.deploy('GovernanceToken', args);
   await task.save({ GovernanceToken: governanceToken.address });
+  await sleep(5000);
+  await task.verify('GovernanceToken', governanceToken.address, args);
 };
--- pkg/deployments/tasks/20230117-weighted-pool-v2/index.ts
+++ pkg/deployments/tasks/20230117-weighted-pool-v2/index.ts
@@ -1,15 +1,19 @@
 import Task from '../../src/task';
 import { TaskRunOptions } from '../../src/types';
+import { sleep } from '../../src/utils';
 import { WeightedPoolDeployment } from './input';
 
 export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
   const input = task.input() as WeightedPoolDeployment;
 
-  const factory = await task.deployAndVerify(
-    'WeightedPoolFactory',
-    [input.Vault, input.ProtocolFeePercentagesProvider, input.initialPauseWindowDuration, input.bufferPeriodDuration],
-    from,
-    force
-  );
+  const args = [
+    input.Vault,
+    input.ProtocolFeePercentagesProvider,
+    input.initialPauseWindowDuration,
+    input.bufferPeriodDuration,
+  ];
+  const factory = await task.deploy('WeightedPoolFactory', args, from, force);
   await task.save({ WeightedPoolFactory: factory.address });
+  await sleep(5000);
+  await task.verify('', factory.address, args);
 };
