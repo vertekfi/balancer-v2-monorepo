16d6f6e4346f4a3f7f9b13649cee5e6a3eba8caf
0xBriz <chaindevone@gmail.com>
2023-01-25 17:32:41 -0500

    voting escrow, ve delegation deployment tasks

--- pkg/deployments/tasks/20230124-voting-escrow/input.ts
+++ pkg/deployments/tasks/20230124-voting-escrow/input.ts
@@ -7,8 +7,8 @@ export type GaugeSystemDeployment = {
 
 export default {
   AuthorizerAdaptor: new Task('20230124-authorizers', TaskMode.READ_ONLY),
-  mainnet: {
-    BPT: '', // BPT of the canonical 80-20 BAL-WETH Pool
+  bsc: {
+    BPT: '0xDD64E2EC144571b4320f7BFB14a56b2b2cBF37ad', // BPT of the canonical 80-20 BAL-WETH Pool
   },
   goerli: {
     BPT: '0xD0F30B415C65B99904caF716ABc3da23f57d3cdd', // BPT of an 80-20 BAL-WETH Pool using test BAL
