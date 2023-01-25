a215f217269eea354cfbd8a7285184027ffa9dc3
0xBriz <chaindevone@gmail.com>
2023-01-24 20:30:50 -0500

    minter, gauge factory, delegation deployments

---
+++ pkg/deployments/tasks/20230124-ve-delegation/index.ts
@@ -0,0 +1,14 @@
+import { ZERO_ADDRESS } from '@balancer-labs/v2-helpers/src/constants';
+import Task from '../../src/task';
+import { TaskRunOptions } from '../../src/types';
+import { VotingEscrowDelegationDeployment } from './input';
+
+export default async (task: Task, { force, from }: TaskRunOptions = {}): Promise<void> => {
+  const input = task.input() as VotingEscrowDelegationDeployment;
+
+  const args = [ZERO_ADDRESS, input.VotingEscrow];
+  const votingEscrowDelegation = await task.deployAndVerify('VeBoostV2', args, from);
+
+  const proxyArgs = [input.Vault, input.VotingEscrow, votingEscrowDelegation.address];
+  await task.deployAndVerify('VotingEscrowDelegationProxy', proxyArgs, from, force);
+};
